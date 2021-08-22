6e6f456ae4bfd379e6e0c948ff71b93dd78e1aa3
Gustavo Canedo <gustavo.canedo@betrybe.com>
2021-08-22 02:22:22 -0300

    fix: tab spaces

--- contracts/Tunfo.sol
+++ contracts/Tunfo.sol
@@ -9,57 +9,62 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+import "./Cards.sol";
+
 contract Tunfo is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable, ERC721Burnable {
-    using Counters for Counters.Counter;
+  using Counters for Counters.Counter;
+  using Cards for Cards.Card;
+  Counters.Counter private _tokenIdCounter;
+  constructor() ERC721("Tunfo", "TNF") {}
 
-    Counters.Counter private _tokenIdCounter;
+  function safeMint(address to) public onlyOwner {
+    _safeMint(to, _tokenIdCounter.current());
+    _tokenIdCounter.increment();
+  }
 
-    constructor() ERC721("Tunfo", "TNF") {}
+  function pause() public onlyOwner {
+    _pause();
+  }
 
-    function safeMint(address to) public onlyOwner {
-        _safeMint(to, _tokenIdCounter.current());
-        _tokenIdCounter.increment();
-    }
+  function unpause() public onlyOwner {
+    _unpause();
+  }
 
-    function pause() public onlyOwner {
-        _pause();
-    }
+  function _baseURI() internal pure override returns (string memory) {
+    return "http://google.com/";
+  }
 
-    function unpause() public onlyOwner {
-        _unpause();
-    }
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+    internal
+    whenNotPaused
+    override(ERC721, ERC721Enumerable)
+  {
+    super._beforeTokenTransfer(from, to, tokenId);
+  }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return "http://google.com/";
-    }
+  function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    super._burn(tokenId);
+  }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        whenNotPaused
-        override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    override(ERC721, ERC721URIStorage)
+    returns (string memory)
+  {
+    return super.tokenURI(tokenId);
+  }
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
+  function supportsInterface(bytes4 interfaceId)
+    public
+    view
+    override(ERC721, ERC721Enumerable)
+    returns (bool)
+  {
+    return super.supportsInterface(interfaceId);
+  }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-        public
-        view
-        override(ERC721, ERC721Enumerable)
-        returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
-    }
+  function generateAttributes(bytes32 seed) public pure returns (Cards.Card memory){
+    // return Cards.Card(1,16,10);
+  }
 }
