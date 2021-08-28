8af1f2583b02062d49d9d0d297a216bcee47bdf8
Gustavo Canedo <gustavo.canedo@betrybe.com>
2021-08-28 19:49:32 -0300

    feat: test Tunfo.test

--- test/tunfo.test.js
+++ test/tunfo.test.js
@@ -1,11 +1,24 @@
 const { expect } = require("chai");
 const deploy = require("./utils/deploy");
+const ATTRIBUTES = require("./fixtures/attributes.json");
+const RARITIES = require("./fixtures/rarities.json");
+const TYPES = require("./fixtures/types.json");
+const { ethers, logger } = require("ethers");
 
-describe("Tunfo", function () {
-
-  it("generateAttributes - Generate a randomized token attributes", async function () {
-    // tunfo = await deploy("Tunfo");
-    // seed = 1
-    // expect(await tunfo.generateAttributes(1)).to.equal("Hello, world!");
+describe("Tunfo", () => {
+  it("test", async () => {
+    const tunfo = await deploy("Tunfo");
+    const accounts = await hre.ethers.getSigners();
+    const wal = ethers.Wallet.createRandom().connect(accounts[0].provider)
+    console.log((await accounts[0].getBalance()).toString());
+    console.log((await wal.getBalance()).toString());
+    await accounts[0].sendTransaction({
+      to: wal.address,
+      value: ethers.utils.parseEther("1") // 1 ether
+    })
+    console.log('----');
+    console.log((await accounts[0].getBalance()).toString());
+    console.log((await wal.getBalance()).toString());
+    console.log(ethers.utils.formatEther(await accounts[0].getBalance()));
   });
 });
