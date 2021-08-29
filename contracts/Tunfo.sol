907032534403395b8c737d119d1a382666480788
Gustavo Canedo <gustavo.canedo@betrybe.com>
2021-08-29 18:58:24 -0300

    feat: test fee

--- contracts/Tunfo.sol
+++ contracts/Tunfo.sol
@@ -27,7 +27,7 @@ contract Tunfo is ERC721, ERC721Enumerable, ERC721URIStorage, Pausable, Ownable,
   function mint() public payable {
     require(msg.value == mintCardCost, "Card cost is 10.");
 
-    // _mintCard(msg.sender);
+    _mintCard(msg.sender);
   }
 
   function _mintCard(address to) private {
--- test/tunfo.test.js
+++ test/tunfo.test.js
@@ -3,42 +3,60 @@ const deploy = require("./utils/deploy");
 const ATTRIBUTES = require("./fixtures/attributes.json");
 const RARITIES = require("./fixtures/rarities.json");
 const TYPES = require("./fixtures/types.json");
+const { cardCost } = require("./fixtures/constants.json");
 const { ethers } = require("ethers");
 
 describe("Tunfo", () => {
   describe("safeMint", () => {
     it("only owner can call safeMint for free", async () => {
       const tunfo = await deploy("Tunfo");
-      const accounts = await hre.ethers.getSigners();
+      const [owner, wallet] = await hre.ethers.getSigners();
 
-      expect(tunfo.safeMint(accounts[1].address)).to.not.be.reverted
-      expect(tunfo.connect(accounts[1]).safeMint(accounts[1].address)).to.be.reverted
+      expect(tunfo.safeMint(wallet.address)).to.not.be.reverted
+      expect(tunfo.connect(wallet).safeMint(wallet.address)).to.be.reverted
     });
   });
 
   describe("mint", () => {
     it("reverts if value is not the Card Cost", async () => {
       const tunfo = await deploy("Tunfo");
-      const accounts = await hre.ethers.getSigners();
+      const [owner, wallet] = await hre.ethers.getSigners();
 
       const provider = tunfo.provider;
       console.log((await provider.getBalance(tunfo.address)).toString());
 
-      await expect(tunfo.mint({value: 1})).to.be.reverted
-      await expect(tunfo.mint({value: 10})).to.not.be.reverted
-      await expect(tunfo.mint({value: 11})).to.be.reverted
+      await expect(tunfo.mint({value: 0})).to.be.reverted;
+      await expect(tunfo.mint({value: cardCost - 1})).to.be.reverted;
+      await expect(tunfo.mint({value: cardCost})).to.not.be.reverted;
+      await expect(tunfo.mint({value: cardCost+1})).to.be.reverted;
       console.log((await provider.getBalance(tunfo.address)).toString());
 
-      await expect(tunfo.connect(accounts[1]).mint({value: 1})).to.be.reverted
-      await expect(tunfo.connect(accounts[1]).mint({value: 11})).to.be.reverted
-      await expect(tunfo.connect(accounts[1]).mint({value: 10})).to.not.be.reverted
+      await expect(tunfo.connect(wallet).mint({value: cardCost-1})).to.be.reverted;
+      await expect(tunfo.connect(wallet).mint({value: cardCost+1})).to.be.reverted;
+      await expect(tunfo.connect(wallet).mint({value: cardCost})).to.not.be.reverted;
       console.log((await provider.getBalance(tunfo.address)).toString());
     });
 
-    it("call _mint with sender address", async () => {
+    it("add a token", async () => {
       const tunfo = await deploy("Tunfo");
-      const accounts = await hre.ethers.getSigners();
-      await tunfo.mint({value: 10});
+      const [owner] = await hre.ethers.getSigners();
+
+      const previousBalance = Number.parseInt(await tunfo.balanceOf(owner.address));
+      await tunfo.mint({value: cardCost});
+      const newBalance = Number.parseInt(await tunfo.balanceOf(owner.address));
+      expect(newBalance).to.equal(previousBalance + 1);
+    });
+
+    it("receives the mint fee", async () => {
+      const tunfo = await deploy("Tunfo");
+      // const provider = tunfo.provider;
+      // const previousBalance = Number.parseInt(await provider.getBalance(tunfo.address));
+      // await tunfo.mint({value: cardCost});
+      // const newBalance = Number.parseInt(await provider.getBalance(tunfo.address));
+      // expect(newBalance).to.equal(previousBalance + cardCost);
+
+      await expect(() => tunfo.mint({value: cardCost}))
+        .to.changeEtherBalance(tunfo, cardCost);
     });
   });
 });
