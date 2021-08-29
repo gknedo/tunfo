63c6d6a2e13e68c83426a98fffd8066dc33d31be
Gustavo Canedo <gustavo.canedo@betrybe.com>
2021-08-29 08:39:57 -0300

    feat: add basic ownership testint

--- test/tunfo.test.js
+++ test/tunfo.test.js
@@ -6,19 +6,13 @@ const TYPES = require("./fixtures/types.json");
 const { ethers, logger } = require("ethers");
 
 describe("Tunfo", () => {
-  it("test", async () => {
-    const tunfo = await deploy("Tunfo");
-    const accounts = await hre.ethers.getSigners();
-    const wal = ethers.Wallet.createRandom().connect(accounts[0].provider)
-    console.log((await accounts[0].getBalance()).toString());
-    console.log((await wal.getBalance()).toString());
-    await accounts[0].sendTransaction({
-      to: wal.address,
-      value: ethers.utils.parseEther("1") // 1 ether
-    })
-    console.log('----');
-    console.log((await accounts[0].getBalance()).toString());
-    console.log((await wal.getBalance()).toString());
-    console.log(ethers.utils.formatEther(await accounts[0].getBalance()));
-  });
+  describe("safeMint", () => {
+    it("only owner can call safeMint freely", async () => {
+      const tunfo = await deploy("Tunfo");
+      const accounts = await hre.ethers.getSigners();
+
+      expect(tunfo.safeMint(accounts[1].address)).to.not.be.reverted
+      expect(tunfo.connect(accounts[1]).safeMint(accounts[1].address)).to.be.reverted
+    });
+  })
 });
