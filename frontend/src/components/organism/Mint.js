65950584535e78dd94c5a3a67c3fafc515ec3834
Gustavo Canedo <gustavo.canedo@betrybe.com>
2021-09-02 19:18:28 -0300

    feat: mint

--- frontend/src/components/organism/Mint.js
+++ frontend/src/components/organism/Mint.js
@@ -1,15 +1,19 @@
 import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
+import specs from "./Tunfo.json";
 
 function Mint({useGlobalState}) {
   const [wallet] = useGlobalState('wallet');
+  const [contract, setContract] = useState();
+
+  useEffect(() => {
+    setContract(new ethers.Contract(specs.address, specs.abi, wallet));
+  }, [wallet, setContract]);
 
   const mint = async (wallet) => {
-    const transaction = {
-      to: "0xc8C14a050D33A178DEbF2D92EaEb5784D13C83e2",
-      value: ethers.utils.parseEther("100"),
-    };
-
-    await wallet.sendTransaction(transaction);
+    const address = await wallet.getAddress();
+    console.log(address);
+    await contract.safeMint(address);
   }
 
   return (
