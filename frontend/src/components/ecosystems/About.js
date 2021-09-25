9d67bdbf0361340e3e4137008da3900adc25470d
Gustavo Canedo <gustavo.canedo@betrybe.com>
2021-09-25 07:17:56 -0300

    centralize text on blocks

--- frontend/src/components/ecosystems/About.js
+++ frontend/src/components/ecosystems/About.js
@@ -6,7 +6,7 @@ function About() {
     <div className="mt-28 flex flex-wrap justify-center content-center">
       <div className="flex flex-wrap align-center justify-center px-40">
         <img className="w-60" src={PangoImage} alt={"Pango"}/>
-        <div className="max-w-screen-sm flex flex-wrap justify-center flex-col gap-y-2">
+        <div className="max-w-screen-sm flex flex-wrap justify-center flex-col gap-y-2 text-center">
           <h2 className="text-5xl">A wildlife clash is happening on your blockchain.</h2>
           <p>Join now in the community of the most innovative card game on GO Chain</p>
         </div>
